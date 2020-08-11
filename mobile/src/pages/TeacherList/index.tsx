538ff3822ea3094d70c9238101d8f1f58a3ab8cb
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-11 13:30:47 -0300

    page header filters style done

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
+import { Picker } from '@react-native-community/picker'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 
 import styles from './styles'
-import { ScrollView } from 'react-native-gesture-handler'
 
 function TeacherList() {
+    
+
     return (
         <View style={styles.container}>
-            <PageHeader title='Proffys disponíveis' />
+            <PageHeader title='Proffys disponíveis'>
+                <View style={styles.searchForm}>
+                    <Text style={styles.label}>Matéria</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder={'Qual a matéria?'}
+                        placeholderTextColor='#c1bccc'
+                    />
+
+                    <View style={styles.inputGroup}>
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Dia da semana</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Qual o dia?'
+                                placeholderTextColor='#c1bccc'
+                            />
+                        </View>
+
+                        <View style={styles.inputBlock}>
+                            <Text style={styles.label}>Horário</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Qual horário?'
+                                placeholderTextColor='#c1bccc'
+                            />
+                        </View>
+                    </View>
+                </View>
+            </PageHeader>
 
             <ScrollView 
                 style={styles.teacherList}
