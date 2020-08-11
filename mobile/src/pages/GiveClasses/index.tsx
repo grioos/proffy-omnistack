6ad4be82c7bab4c079f17df7bd18ec0df09e6f39
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-11 10:08:37 -0300

    give classes page created

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import styles from '../Landing/styles'
+
+function GiveClasses() {
+    return (
+        <View style={styles.container} />
+    ) 
+}
+
+export default GiveClasses
