cf292396040580384d8d85f68e80038ef16805c0
Gabriel Rios <60109015+grioos@users.noreply.github.com>
2020-08-11 13:46:05 -0300

    filter hide and seek

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text } from 'react-native'
-import { ScrollView, TextInput } from 'react-native-gesture-handler'
-import { Picker } from '@react-native-community/picker'
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
@@ -9,39 +9,56 @@ import TeacherItem from '../../components/TeacherItem'
 import styles from './styles'
 
 function TeacherList() {
-    
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isFiltersVisible)
+    }
 
     return (
         <View style={styles.container}>
-            <PageHeader title='Proffys disponíveis'>
-                <View style={styles.searchForm}>
-                    <Text style={styles.label}>Matéria</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Qual a matéria?'}
-                        placeholderTextColor='#c1bccc'
-                    />
+            <PageHeader 
+                title='Proffys disponíveis'
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name='filter' size={20} color='#FFF' />
+                    </BorderlessButton>
+                )}                
+            >
+                { isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder={'Qual a matéria?'}
+                            placeholderTextColor='#c1bccc'
+                        />
 
-                    <View style={styles.inputGroup}>
-                        <View style={styles.inputBlock}>
-                            <Text style={styles.label}>Dia da semana</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Qual o dia?'
-                                placeholderTextColor='#c1bccc'
-                            />
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Dia da semana</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder='Qual o dia?'
+                                    placeholderTextColor='#c1bccc'
+                                />
+                            </View>
+
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder='Qual horário?'
+                                    placeholderTextColor='#c1bccc'
+                                />
+                            </View>
                         </View>
 
-                        <View style={styles.inputBlock}>
-                            <Text style={styles.label}>Horário</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Qual horário?'
-                                placeholderTextColor='#c1bccc'
-                            />
-                        </View>
+                        <RectButton style={styles.submitButton} >
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
                     </View>
-                </View>
+                )}
             </PageHeader>
 
             <ScrollView 
